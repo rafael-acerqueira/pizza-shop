359ff8161c1a9bd4f6bd2f47aa9a02712de90863
Rafael Aquino <rafael.acerqueirah@gmail.com>
2024-05-14 11:10:00 -0300

    Add styles to Login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import './global.css'
+
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
-import './global.css'
+
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 
 function App() {
