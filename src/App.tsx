72fd80e73333a74cb6d6030e8f28b107c02c58bc
Rafael Aquino <rafael.acerqueirah@gmail.com>
2024-05-16 17:36:44 -0300

    Install axios and  react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './global.css'
 
+import { QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 
 import { Toaster } from 'sonner'
 import { ThemeProvider } from './components/theme/theme-provider'
+import { queryClient } from './lib/react-query'
 
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 
@@ -15,7 +17,9 @@ function App() {
         <Helmet titleTemplate="%s | Pizza Shop" />
 
         <Toaster richColors />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   )
