5ee53188fdb5229823fa439a744e1be283f7202c
Rafael Aquino <rafael.acerqueirah@gmail.com>
2024-05-16 16:08:22 -0300

    Create a custom not found page

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,11 +6,13 @@ import { SignUp } from './pages/auth/sign-up'
 import { Orders } from './pages/app/orders/orders'
 import { AppLayout } from './pages/layouts/app'
 import { AuthLayout } from './pages/layouts/auth'
+import { NotFound } from './pages/404'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       {
         path: '/',
