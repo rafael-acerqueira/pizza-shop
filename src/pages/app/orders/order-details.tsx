dedac06038f524e7abcce07d3732aab94fe5c6da
Rafael Aquino <rafael.acerqueirah@gmail.com>
2024-05-22 11:39:12 -0300

    Add loading status to order details modal

--- src/pages/app/orders/order-details.tsx
+++ src/pages/app/orders/order-details.tsx
@@ -21,6 +21,7 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
+import { OrderDetailsSkeleton } from './order-details-skeleton'
 
 
 export interface OrderDetailsProps {
@@ -43,7 +44,7 @@ export function OrderDetails({ orderId, open }: OrderDetailsProps) {
       </DialogHeader>
 
       {
-        order && (
+        order ? (
           <div className="space-y-6">
             <Table>
               <TableBody>
@@ -132,6 +133,8 @@ export function OrderDetails({ orderId, open }: OrderDetailsProps) {
               </TableFooter>
             </Table>
           </div>
+        ) : (
+          <OrderDetailsSkeleton />
         )}
     </DialogContent>
   )
